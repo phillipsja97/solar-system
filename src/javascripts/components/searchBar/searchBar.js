c41abaf661537b995de2799781ecd80570939e00
Jamie <phillipsja97@gmail.com>
2019-10-30 11:34:31 -0500

    fixed everything. fully functional

--- src/javascripts/components/searchBar/searchBar.js
+++ src/javascripts/components/searchBar/searchBar.js
@@ -1,27 +1,15 @@
 import $ from 'jquery';
-// import print from '../printPlanets/printPlanets';
-import planet from '../../helpers/data/planets';
-// import print from '../printPlanets/printPlanets';
 
 const searchBar = () => {
-  const planets = planet.getPlanets();
   $('.searchBar').on('keyup', () => {
     const input = $('#input').val().toLowerCase();
-    console.log(input);
-    const planetNames = planets.filter((x) => x.name.toLowerCase().includes(input));
-    for (let i = 0; i < planetNames.length; i += 1) {
-      // for (let j = 0; i < planets[j].length; i += 1) {
-      $('.planet-card').each(() => {
-        console.log($(this));
-        if ($(this).attr('id') === planetNames[i].name) {
-          $(this).show();
-        } else {
-          $(this).hide();
-        }
-      });
-    }
-    // set up a conditional to check if Planets.name === planetNames.name, if so show. if not, hide.
-    // may need to set up a double loop through planets to be able to check  the name match.
+    $('.planet-card').each((index, card) => {
+      if (card.id.toLowerCase().includes(input)) {
+        $(card).show();
+      } else {
+        $(card).hide();
+      }
+    });
   });
 };
 
